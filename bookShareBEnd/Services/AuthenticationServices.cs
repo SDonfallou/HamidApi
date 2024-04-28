81ca5c58fde1d82321b3f3e426aa945dfb58615e
Donfallou <littlefallou@gmail.com>
2024-04-28 17:53:25 +0200

    Fix(Claim) : added userId

--- bookShareBEnd/Services/AuthenticationServices.cs
+++ bookShareBEnd/Services/AuthenticationServices.cs
@@ -44,13 +44,15 @@ namespace bookShareBEnd.Services
             var securityKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration["Jwt:key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
+      
+
             var claims = new[]
-                    {
-                       new Claim(ClaimTypes.NameIdentifier, user.Name),
-                       new Claim(ClaimTypes.Email, user.Email),
-                       new Claim(ClaimTypes.Role, user.RoleId.ToString()), // Assuming RoleId is a Guid
-                       
-                     };
+                          {
+                                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()), // Assuming UserId is a Guid or an integer
+                                new Claim(ClaimTypes.Name, user.Name),
+                                new Claim(ClaimTypes.Email, user.Email),
+                                new Claim(ClaimTypes.Role, user.RoleId.ToString()), // Assuming RoleId is a Guid
+                            };
             var token = new JwtSecurityToken(
                               issuer: _configuration["Jwt:Issuer"],
                               audience: _configuration["Jwt:Audience"],
